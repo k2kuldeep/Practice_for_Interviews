4820729ec493bce8d2f08bfd698e21f3d00a7e53
pk7856 <pksharma7856@gmail.com>
2019-06-03 23:23:09 +0530

    30/5/19

---
+++ Solution3.java
@@ -0,0 +1,38 @@
+/*Problem Statement : Find the nth number of the series. Series is a defined as
+sum of unique powers of 5 starting from 1.
+5 , 25 , 30(5+25) , 125 , 130(125+5) , 150(125+25) , 155(125+25+5)...... 
+
+Follow-Up Question 1: Use Constant Space ie, no extra array
+Follow-Up Question 2: Try doing it in O(log n)
+*/
+import java.util.*;
+
+class Solution3
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		//int k = sc.nextInt();
+		int x = 6;
+		
+		String num = "";
+		while(x != 0){
+			num = num + (x%2);
+			x = x/2;
+		}
+		
+		//System.out.println(num);
+		
+		double sum = 0;
+		for(int i=num.length();i>0;i--){
+			//System.out.println(num.charAt(i-1) + " \t" + Math.pow(5,i));
+			int z = Character.getNumericValue(num.charAt(i-1));
+			
+			sum = sum +  z * Math.pow(5,i);
+			//System.out.println(sum);
+		}
+		System.out.println(sum);
+	}
+}
+
