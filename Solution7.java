fe6e363849ea42bb57d0e81196a08a4c4baa2bcc
pk7856 <pksharma7856@gmail.com>
2019-06-08 12:11:54 +0530

    updated

--- Solution7.java
+++ Solution7.java
@@ -19,8 +19,14 @@ public class Solution7 {
 		stack.push(9);
 		
 		System.out.println("before : "+stack);
-		/*Stack<Integer> tmpstack = new Stack<>();
 		
+		//taking each element out of stack into "temp" variable one by one and putting into temporary stack 
+		//until an element is found which is smaller than the top value of temporary stack.
+		//if top(temporary stack) < temp(which was the top value of stack), then temporary stack values are popped
+		//and stored to stack until the element is found which is less than temp value.
+		//when the element which is less than temp value is found, then temp value is stored above it.
+		
+		/*Stack<Integer> tmpstack = new Stack<>();
 		while(!stack.isEmpty()){
 			int temp = stack.pop();
 			
@@ -31,14 +37,39 @@ public class Solution7 {
 			
 		}
 		
-		System.out.println("after = "+tmpstack);*/
+		System.out.println("after = "+tmpstack);
+		*/
 		
-		
-		
+		sortstack(stack);
+		System.out.println("after = "+stack);
 	}
 	
-	public static void sortstack(){
+	//pop out elements from stack into temp variable until stack is empty
+	//then pass it in backward to sortinsert function
+	//it will put the variable at its right position in the stack
+	
+	public static void sortstack(Stack<Integer> s){
+		if(!s.isEmpty()){
+			int temp = s.pop();
+			sortstack(s);
+			sortInsert(s,temp);
+		}	
+	}
+	
+	public static void sortInsert(Stack<Integer> stack,int temp){
+		//if top(stack) < temp then push temp and return
+		if(stack.isEmpty() || stack.peek()<temp)		// top(stack)<temp for ascending
+		{
+			stack.push(temp);
+			return;
+		}
 		
+		// if top of stack is > temp
+		// pop top until less than temp value is found and then push it .
+		int top = stack.pop();
+		sortInsert(stack, temp);
+		
+		stack.push(top);	
 	}
 
 }
