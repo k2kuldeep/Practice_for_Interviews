0fb8e665dce8326c82b7f40a4cee97403b55ebd6
pk7856 <pksharma7856@gmail.com>
2019-06-13 00:38:26 +0530

    commit

---
+++ Solution2b.java
@@ -0,0 +1,11 @@
+/*Write a program that checks whether a given Linked List contains loop .
+ Follow up :  if loop is present then removes the loop. 
+ */
+public class Solution2b {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
