79ced764bcfa230dc9140703734be0b37985b12d
pk7856 <pksharma7856@gmail.com>
2019-10-19 01:17:04 +0530

    Match Function of String

---
+++ StringMatchFunction.java
@@ -0,0 +1,12 @@
+public class StringMatchFunction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = "11111";
+		System.out.print(str.matches("(.*)101(.*)"));
+		
+				
+	}
+	
+}
