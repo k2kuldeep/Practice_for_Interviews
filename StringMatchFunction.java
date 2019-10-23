e4d77ebd134cca84145ce2956a609f4374dfdf2b
pk7856 <pksharma7856@gmail.com>
2019-10-23 22:20:06 +0530

    String Functions : match and base change

--- StringMatchFunction.java
+++ StringMatchFunction.java
@@ -1,3 +1,5 @@
+//To use BigInteger must import below one.
+import java.math.BigInteger;
 public class StringMatchFunction {
 
 	public static void main(String[] args) {
@@ -6,7 +8,14 @@ public class StringMatchFunction {
 		String str = "11111";
 		System.out.print(str.matches("(.*)101(.*)"));
 		
-				
+		//-----------------------------------------------------------------------------------------------------------------
+		//Base Change Program. All data type have base 10only. converting to base 16.
+		int x = 145;
+		
+		BigInteger br = new BigInteger(x+"",10);//10 is the initial base
+		String s = br.toString(16);//16 is the base into which converted
+		
+		System.out.println(s);
 	}
 	
 }
