8de2796b0db16c7131209d8dc55247b24468b764
Pranav <34658584+pk7856@users.noreply.github.com>
2020-05-02 13:32:20 +0530

    find the number that occur once

---
+++ leetcode/singleNumber.java
@@ -0,0 +1,30 @@
+/*Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+Example 1:
+Input: [2,2,1]
+Output: 1
+Example 2:
+
+Input: [4,1,2,1,2]
+Output: 4
+ */
+ 
+public class singleNumber {
+    public static void main(String[] args) {
+        int[] nums = {2,2,1,1,4,5,6,7,5,6,7,};
+
+        int res = nums[0];
+        for(int i=1;i<nums.length;i++)
+            res ^= nums[i];
+
+        System.out.println(res);
+
+    }
+
+}
+NOTE:
+1. method1 : put in hasmap and then count. 
+2. method2 :sort and check if consecutive numbers are same.
+3. XOR of a number with itself is 0.
+4. XOR of a number with 0 is number itself.
